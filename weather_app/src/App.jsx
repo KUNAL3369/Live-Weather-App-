efd8ac23b506462851931ab9be23dbc57dabbee3
kunal <kunal.prabhakar3369@gmail.com>
2025-08-08 13:29:16 +0545

    Your comment

---
+++ weather_app/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import WeatherApp from './components/WeatherApp'
+import './components/WeatherApp.css'
+
+ const App = () => {
+  return (
+    <div><WeatherApp /></div>
+  )
+}
+export default App
